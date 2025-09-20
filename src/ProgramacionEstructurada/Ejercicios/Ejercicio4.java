b80339b84c11be41879bd68a80ecf1adc5f5b4c5
neferasx <aly.maria2ooo1@gmail.com>
2025-09-20 22:59:26 +0200

    Corrigiendo el ejercicio 4.

--- src/ProgramacionEstructurada/Ejercicios/Ejercicio4.java
+++ src/ProgramacionEstructurada/Ejercicios/Ejercicio4.java
@@ -11,30 +11,32 @@ public class Ejercicio4 {
         double tasaDolar = 122.499;
         double tasaDracmasGriegas = 88.607 / 100;
         double tasaLirasItalianas =  9.289 / 100;
+		double tasaFrancosFranceses = 20.110;
 
         double chelinesAustriacos, dolar, dracmasGriegas, lirasItalianas, pesetas;
 
         Scanner teclado = new Scanner(System.in);
 
 		// a.- Se introduce por teclado una cantidad en Chelines Austriacos y se visualiza su equivalente en Pesetas.
-		System.out.println("Introduce una cantidad en Chelines Austriacos: ");
+		System.out.print("Introduce una cantidad en Chelines Austriacos: ");
 		chelinesAustriacos = teclado.nextDouble();
-		double pesetasChelinesA = chelinesAustriacos * tasaChelinesAustriacos;
-		System.out.println("Su equivalencia a pesetas son: " + pesetasChelinesA);
+		double pesetasChelines = chelinesAustriacos * tasaChelinesAustriacos;
+		System.out.println(chelinesAustriacos + " Chelines Austriacos = " + pesetasChelines + " Pesetas");
 		
 		// b.- Se introduce por teclado una cantidad en Dracmas Griegas y se visualiza su equivalente en Francos Franceses
 		System.out.println("Introduce una cantidad en Dracmas Griegas: ");
 		dracmasGriegas = teclado.nextDouble();
-		double pesetasDracmasG = dracmasGriegas * tasaDracmasGriegas; 
-		System.out.println("Su equivalencia a pesetas son: " + pesetasDracmasG);
+		double pesetasDracmas = dracmasGriegas * tasaDracmasGriegas;           // Dracmas → Pesetas
+		double francosFranceses = pesetasDracmas / tasaFrancosFranceses;       // Pesetas → Francos Franceses
+		System.out.println(dracmasGriegas + " Dracmas Griegas = " + francosFranceses + " Francos Franceses");
 		
 		// c.- Se introduce por teclado una cantidad en Pesetas y se visualiza su equivalente en Dólares y Liras Italianas.
-		System.out.println("Introduce una cantidad en pesetas: ");
+		System.out.print("Introduce una cantidad en Pesetas: ");
 		pesetas = teclado.nextDouble();
-		dolar = pesetas / tasaDolar; 
-		lirasItalianas = pesetas / tasaLirasItalianas; 
-		System.out.println("Su equivalencia en dólares son: " + dolar);
-		System.out.println("Su equivalencia en Liras Italianas son: " + lirasItalianas);
+		dolar = pesetas / tasaDolar;
+		lirasItalianas = pesetas / tasaLirasItalianas;
+		System.out.println(pesetas + " Pesetas = " + dolar + " Dólares");
+		System.out.println(pesetas + " Pesetas = " + lirasItalianas + " Liras Italianas");
 		
 		teclado.close();
 
