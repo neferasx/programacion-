5425b204b2f69b4c4425b25fb64c740f7f7d87e6
Alina Maria Stroie <165716672+neferasx@users.noreply.github.com>
2025-09-24 09:08:40 +0200

    Deberes java 23 - 09 - 2025 pero corregido

--- src/ProgramacionEstructurada/Ejercicios/Ejercicio13.java
+++ src/ProgramacionEstructurada/Ejercicios/Ejercicio13.java
@@ -1,21 +1,21 @@
-package ProgramacionEstructurada.Ejercicios;
+package Ejercicios;
 
 import java.util.Scanner;
 
 public class Ejercicio13 {
     public static void main(String[] args) {
 
-        int numero, resultado=0, contador = 1;
+        int numero, resultado, contador = 1;
 
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Introduce un número del 1 al 9: ");
         do {
-            numero = teclado.nextInt();
             System.out.println("Debes introducir un número del 1 al 9: ");
+            numero = teclado.nextInt();
         } while (numero<1 || numero>9);
 
-        while(resultado<numero * 10) {
+        while (contador <= 10) {
             resultado = numero * contador;
             System.out.println(numero + " * " + contador + " = " + resultado);
             contador++;
