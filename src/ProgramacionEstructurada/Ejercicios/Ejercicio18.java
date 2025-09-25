d61f3d0601eb70594f0fd9c8609e6d582f7f8d33
Alina Maria Stroie <165716672+neferasx@users.noreply.github.com>
2025-09-25 13:24:54 +0200

    Ejercicios corregidos deberes 24 - 09 - 2025

--- src/ProgramacionEstructurada/Ejercicios/Ejercicio18.java
+++ src/ProgramacionEstructurada/Ejercicios/Ejercicio18.java
@@ -1,19 +1,39 @@
-package ProgramacionEstructurada.Ejercicios;
+package Ejercicios;
 
 public class Ejercicio18 {
-    public static void main (String[] args) {
-
-        int suma = 0, contador = 1, numero_par;
-
-        while (contador <=10) {
-            numero_par = contador * 2;
-            suma += numero_par;
-            contador++;
-        }
+    public static void main(String[] args) {
+        // Que visualice la suma de los 10 primeros números pares
 
 
-        System.out.println("La suma de los 10 primeros números pares es: " + suma);
+        /*
+            int suma = 0, contador = 1, numero_par;
+
+            while (contador <= 10) {
+                numero_par = contador * 2;
+                suma += numero_par;
+                contador++;
+            }
 
 
+            System.out.println("La suma de los 10 primeros números pares es: " + suma);
+
+
+    }
+}
+*/
+        int contadorPares, numeroPar, suma;
+        final int CANTIDADPARES = 10;
+
+            suma = 0;
+            numeroPar = 2;
+            contadorPares = 0;
+
+            while(contadorPares < CANTIDADPARES) {
+                suma += numeroPar;
+                contadorPares++;
+                numeroPar+=2;
+            }
+
+        System.out.println("La suma es: " + suma);
     }
 }
