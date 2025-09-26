bff716462cc807acd1af1cb53ef25f432255c5f2
Alina Maria Stroie <165716672+neferasx@users.noreply.github.com>
2025-09-26 12:09:13 +0200

    Practicando ejercicios de java 26 - 09 - 2025

---
+++ src/ProgramacionEstructurada/Ejercicios/Ejercicio15.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+
+public class Ejercicio15 {
+    public static void main (String [] args) {
+
+        int contador = 0;
+
+        while(contador <= 100) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
